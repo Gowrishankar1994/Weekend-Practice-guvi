526445ddb3763a67ab20928acefae671d766caf9
Gowrishankar1994 <m.gowrishankar007@gmail.com>
2024-07-14 22:29:04 +0530

    Update strings_to_title_caps.js

--- Day 3/Day 3.2/strings_to_title_caps.js
+++ Day 3/Day 3.2/strings_to_title_caps.js
@@ -1,4 +1,4 @@
-let strArray = ["I","am","suresh","from","chennai"];
+let strArray = ["I","am","gowri","from","chennai"];
 let titleCaps = (array) => {
     let new_array = array.map((x => x = x.charAt(0).toUpperCase() + x.slice(1) ));
     console.log(new_array)
