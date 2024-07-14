8066abaa5cad53bbe09d5958d73867198ec192aa
Gowrishankar1994 <m.gowrishankar007@gmail.com>
2024-07-14 22:25:29 +0530

    Delete Day 3 directory

--- Day 3/strings_to_title_caps.js
+++
@@ -1,9 +0,0 @@
-let strArray = ["I","am","suresh","from","chennai"];
-
-let titleCaps_IIFE = ( function(array) {
-    for(let index in array){
-        let ch = array[index][0].toUpperCase();
-        array[index] = ch + array[index].slice(1);
-    }
-    console.log(array)
-} )(strArray);
